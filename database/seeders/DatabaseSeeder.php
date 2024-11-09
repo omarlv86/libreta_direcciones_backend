24d53b20801242a741ce4cedde3ad7406da7aa42
Ricardo Omar Lugo Vargas <ricardo.lugo@nuvem.mx>
2024-11-09 02:02:02 -0600

    main | Creando array de orden de ejecucion de seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            ContactsTableSeeder::class,
+            MailTableSeeder::class,
+            PhonesTableSeeder::class,
+            AddressTableSeeder::class,
+        ]);
     }
 }
