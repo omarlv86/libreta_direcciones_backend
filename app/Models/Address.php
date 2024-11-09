3d76365bfafb3a4886d46e98b1da5dddde785781
Ricardo Omar Lugo Vargas <ricardo.lugo@nuvem.mx>
2024-11-09 02:45:03 -0600

    main | Creando relacion entre contact y address

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Contact;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
 
 class Address extends Model
 {
     use HasFactory;
+
+    public function contact(): belongsTo
+    {
+        return $this->belongsTo(Contact::class);
+    }
 }
